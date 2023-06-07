6c0b54715102eb0eaafcf3c3fc673cf4260b7985
Foxikle <tristantroop260@gmail.com>
2023-06-07 17:29:43 -0400

    Re organized and renamed

--- src/main/java/dev/foxikle/customnpcs/runnables/CommandRunnable.java
+++ src/main/java/dev/foxikle/customnpcs/runnables/CommandRunnable.java
@@ -1,20 +1,21 @@
-package dev.foxikle.customnpcs;
+package dev.foxikle.customnpcs.runnables;
 
+import dev.foxikle.customnpcs.CustomNPCs;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class ChatRunnable extends BukkitRunnable {
+public class CommandRunnable extends BukkitRunnable {
 
     private Player player;
 
-    public ChatRunnable(Player player){
+    public CommandRunnable(Player player){
         this.player = player;
     }
 
     @Override
     public void run() {
-        if(!CustomNPCs.getInstance().waiting.contains(player))
+        if(!CustomNPCs.getInstance().commandWaiting.contains(player))
             this.cancel();
         player.sendTitle(ChatColor.GOLD + "Type command in chat", ChatColor.RED + "Do not include the slash.", 0, 20, 0);
     }
