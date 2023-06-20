cf51c17231e9e7000e8e246d59b095b66e99cf79
Foxikle <tristantroop260@gmail.com>
2023-06-20 14:19:56 -0400

    Added Action object

---
+++ src/main/java/dev/foxikle/customnpcs/Action.java
@@ -0,0 +1,49 @@
+package dev.foxikle.customnpcs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Action {
+
+    private String subCommand;
+    private ArrayList<String> args;
+
+    public Action(String subCommand, ArrayList<String> args){
+        this.subCommand = subCommand;
+        this.args = args;
+    }
+
+    public List<String> getArgsCopy() {
+        if(!args.isEmpty())
+            return new ArrayList<>(args);
+        else
+            return new ArrayList<>();
+    }
+
+    public ArrayList<String> getArgs() {
+        return args;
+    }
+
+    public String getSubCommand() {
+        return subCommand;
+    }
+
+    public String getCommand(Player player){
+        return "npcaction " + player.getUniqueId() + " " + subCommand + " " + String.join(" ", args);
+    }
+
+    public static Action of(String string){
+        ArrayList<String> split = new ArrayList<>(Arrays.stream(string.split("%::%")).toList());
+        String sub = split.get(0);
+        split.remove(0);
+        return new Action(sub, split);
+    }
+
+    public String toString(){
+        return subCommand + "%::%" + String.join("%::%", args);
+    }
+}
