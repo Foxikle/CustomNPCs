a567bbabf07bba09a2d6f2efb51f9479e7e3f684
Foxikle <tristantroop260@gmail.com>
2023-06-28 15:58:04 -0400

    Fixed bStats not being relocated properly

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     `maven-publish`
     id("io.papermc.paperweight.userdev") version "1.5.5"
     id("xyz.jpenilla.run-paper") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
@@ -16,29 +17,33 @@ dependencies {
 }
 
 group = "dev.foxikle"
-version = "1.3-pre4"
+version = "1.3-pre5"
 description = "CustomNPCs"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
 
-
-
-
 publishing {
-    repositories{
-        mavenLocal()
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Foxikle/CustomNPCs")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
     }
-    publications{
-        create<MavenPublication>("maven") {
+    publications {
+        register<MavenPublication>("gpr") {
             from(components["java"])
         }
     }
-
 }
 
 tasks {
     assemble {
         dependsOn(reobfJar)
+        dependsOn(shadowJar)
     }
 
     compileJava {
@@ -64,6 +69,10 @@ tasks {
 
     reobfJar {
         outputJar.set(layout.buildDirectory.file("C:/Users/tscal/Desktop/bungeeserver/Etheria/plugins/CustomNPCs-${project.version}.jar"))
+    }
+
+    shadowJar {
+        relocate("org.bstats", "dev.foxikle.dependencies.bstats")
     }
 }
 
