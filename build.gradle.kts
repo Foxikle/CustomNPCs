5e3e449611c0d8997a480b6678d43cea2547bed9
Foxikle <tristantroop260@gmail.com>
2023-12-13 20:47:47 -0500

    add 1.20.1 / 1.20 module

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":v1_20_R3", "reobf"))
     implementation(project(":v1_20_R2", "reobf"))
+    implementation(project(":v1_20_R1", "reobf"))
 }
 
 allprojects {
