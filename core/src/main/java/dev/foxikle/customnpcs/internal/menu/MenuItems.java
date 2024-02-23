8aaa898172e132cb876a30fd80de0be053a841ae
foxikle <tristantroop260@gmail.com>
2024-02-23 15:42:33 -0500

    Simplification

--- core/src/main/java/dev/foxikle/customnpcs/internal/menu/MenuItems.java
+++ core/src/main/java/dev/foxikle/customnpcs/internal/menu/MenuItems.java
@@ -2,15 +2,9 @@ package dev.foxikle.customnpcs.internal.menu;
 
 import me.flame.menus.builders.items.ItemBuilder;
 import me.flame.menus.items.MenuItem;
-import me.flame.menus.menu.ActionResponse;
 import org.bukkit.Material;
 
 
 public class MenuItems {
-    public static final MenuItem MENU_GLASS = ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE)
-            .setName(" ")
-            .buildItem((i, event) -> {
-                event.setCancelled(true);
-                return ActionResponse.DONE;
-            });
+    public static final MenuItem MENU_GLASS = ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE).setName(" ").buildItem();
 }
