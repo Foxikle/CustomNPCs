cdf7a88304c6c06cb6a729529ef247f1f1d0d7d3
foxikle <tristantroop260@gmail.com>
2024-09-22 10:26:30 -0400

    Decimal formatting, license, optimizations

--- core/src/main/java/dev/foxikle/customnpcs/internal/utils/Utils.java
+++ core/src/main/java/dev/foxikle/customnpcs/internal/utils/Utils.java
@@ -1,4 +1,26 @@
-package dev.foxikle.customnpcs.internal;
+/*
+ * Copyright (c) 2024. Foxikle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.foxikle.customnpcs.internal.utils;
 
 import dev.foxikle.customnpcs.internal.interfaces.InternalNpc;
 import net.kyori.adventure.text.Component;
@@ -8,6 +30,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +39,11 @@ import java.util.List;
  */
 public class Utils {
 
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#");
+
     /**
      * Creates a MUTABLE list
+     *
      * @param vararg The vararg of emelments to be added to a mutable list
      * @return The mutable list containing the passed elements.
      */
@@ -33,6 +59,7 @@ public class Utils {
 
     /**
      * Gets the total number of xp points at a level
+     *
      * @param level the level to get the number of points
      * @return the number of points at a specific level
      */
@@ -51,6 +78,7 @@ public class Utils {
 
     /**
      * Gets the total experience of a player
+     *
      * @param player the player
      * @return the number of experience points the player has
      */
@@ -60,6 +88,7 @@ public class Utils {
 
     /**
      * Sets the player's total experience, including levels
+     *
      * @param player the player
      * @param amount the amount of xp
      */
@@ -91,6 +120,7 @@ public class Utils {
 
     /**
      * Calculates the Location for the NPCs to look at.
+     *
      * @param npc the NPC
      * @return the location to look at
      */
@@ -111,6 +141,8 @@ public class Utils {
     }
 
     public static Component mm(String str) {
+        if (str.isBlank() || str.isEmpty()) return Component.empty();
         return MiniMessage.miniMessage().deserialize(str).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
     }
+
 }
