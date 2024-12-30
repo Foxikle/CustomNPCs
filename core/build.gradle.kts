a9260ad36eb3fda0416af716b4fba5d42b46da24
foxikle <tristantroop260@gmail.com>
2024-12-29 19:02:49 -0500

    Update dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -40,8 +40,8 @@ dependencies {
     compileOnly("org.mineskin:java-client:1.2.4-SNAPSHOT")
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
-    compileOnly("dev.velix:imperat-core:1.3.2")
-    compileOnly("dev.velix:imperat-bukkit:1.3.2")
+    compileOnly("dev.velix:imperat-core:1.5.1")
+    compileOnly("dev.velix:imperat-bukkit:1.5.1")
 }
 
 tasks {
