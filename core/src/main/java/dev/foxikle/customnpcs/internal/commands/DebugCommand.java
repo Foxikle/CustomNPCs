a5d617d9331c6d7926205881da8bf82223c484d2
foxikle <foxikle@cytonic.net>
2025-03-09 15:24:38 -0400

    Create new debug command

---
+++ core/src/main/java/dev/foxikle/customnpcs/internal/commands/DebugCommand.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025. Foxikle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.foxikle.customnpcs.internal.commands;
+
+import dev.foxikle.customnpcs.internal.CustomNPCs;
+import dev.foxikle.customnpcs.internal.utils.Msg;
+import dev.velix.imperat.BukkitSource;
+import dev.velix.imperat.annotations.Description;
+import dev.velix.imperat.annotations.Permission;
+import dev.velix.imperat.annotations.SubCommand;
+import dev.velix.imperat.annotations.Usage;
+
+@Permission("customnpcs.edit")
+@SubCommand(value = "debug", attachDirectly = true)
+@Description("Enables debug message. (They can be very spammy)")
+public class DebugCommand {
+    @Usage
+    public void execute(BukkitSource sender) {
+        CustomNPCs plugin = CustomNPCs.getInstance();
+        if (plugin.isDebug()) {
+            sender.reply(Msg.translate(CommandUtils.getLocale(sender), "customnpcs.commands.debug.message.disable"));
+            plugin.setDebug(false);
+        } else {
+            sender.reply(Msg.translate(CommandUtils.getLocale(sender), "customnpcs.commands.debug.message.enable"));
+            plugin.setDebug(true);
+        }
+    }
+}
