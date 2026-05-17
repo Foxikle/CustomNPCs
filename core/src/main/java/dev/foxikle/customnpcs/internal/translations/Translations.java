19d440557a95bdef06395ef6cb6b4a1e730bd2d9
foxikle <foxikle@cytonic.net>
2026-05-17 11:02:36 -0400

    Update localization

--- core/src/main/java/dev/foxikle/customnpcs/internal/translations/Translations.java
+++ core/src/main/java/dev/foxikle/customnpcs/internal/translations/Translations.java
@@ -23,29 +23,35 @@
 package dev.foxikle.customnpcs.internal.translations;
 
 import net.kyori.adventure.key.Key;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationStore;
 
-import java.util.List;
+import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Translations {
     public static final Locale VIETNAMESE = new Locale("vi");
     public static final Locale RUSSIAN = new Locale("ru");
-    TranslationStore<Component> registry = TranslationStore.component(Key.key("customnpcs:localization"));
+    private static final TranslationStore<MessageFormat> STORE = TranslationStore.messageFormat(Key.key("customnpcs" +
+            ":root"));
+    private static final Map<Locale, ResourceBundle> BUNDLES = new HashMap<>();
+    private static boolean setup = false;
 
     public void setup() {
-        List.of(
-                ResourceBundle.getBundle("localization.Chinese", Locale.SIMPLIFIED_CHINESE),
-                ResourceBundle.getBundle("localization.Russian", RUSSIAN),
-                ResourceBundle.getBundle("localization.German", Locale.GERMAN),
-                ResourceBundle.getBundle("localization.English", Locale.US),
-                ResourceBundle.getBundle("localization.Vietnamese", VIETNAMESE)
-        ).forEach(b -> registry.registerAll(b.getLocale(), b.keySet(), s -> Component.translatable(b.getString(s))));
+        if (setup) return;
+        BUNDLES.put(Locale.SIMPLIFIED_CHINESE, ResourceBundle.getBundle("localization.Chinese",
+                Locale.SIMPLIFIED_CHINESE));
+        BUNDLES.put(RUSSIAN, ResourceBundle.getBundle("localization.Russian", RUSSIAN));
+        BUNDLES.put(Locale.GERMAN, ResourceBundle.getBundle("localization.German", Locale.GERMAN));
+        BUNDLES.put(Locale.US, ResourceBundle.getBundle("localization.English", Locale.US));
+        BUNDLES.put(VIETNAMESE, ResourceBundle.getBundle("localization.Vietnamese", VIETNAMESE));
 
-        registry.defaultLocale(Locale.ENGLISH);
-        GlobalTranslator.translator().addSource(registry);
+        BUNDLES.forEach((locale, b) -> STORE.registerAll(locale, b.keySet(), s -> new MessageFormat(b.getString(s))));
+
+        GlobalTranslator.translator().addSource(STORE);
+        setup = true;
     }
 }
