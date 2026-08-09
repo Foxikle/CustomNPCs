7c05df4016fbf0e0b603e68a24fa77cf465c198b
foxikle <foxikle@cytonic.net>
2026-08-09 14:37:52 -0400

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(project(":v1_20_R4", configuration = "default"))
 }
 
-var pluginVersion = "1.8.0"
+var pluginVersion = "1.8.1"
 
 allprojects {
     group = "dev.foxikle"
